3a8f3fe2a2f88f18a7f0f0db2b34f6158b385012
launchpad5682 <launchpad5682@gmail.com>
2022-07-30 17:44:46 +0530

    feat: Drawer added

---
+++ src/common/Drawer/index.jsx
@@ -0,0 +1,46 @@
+import { useDrawerContext } from "../../context/drawer-context";
+import styles from "./index.module.css";
+
+const Drawer = ({ children }) => {
+  const { drawerToggle } = useDrawerContext();
+  return (
+    <aside
+      className={`${styles.drawer} ${
+        drawerToggle ? styles.drawer__on : styles.drawer__off
+      }`}
+    >
+      {children}
+    </aside>
+  );
+};
+
+const Toggle = () => {
+  const { drawerToggle, setDrawerToggle } = useDrawerContext();
+
+  return (
+    <span
+      onClick={() => setDrawerToggle((prev) => !prev)}
+      className={styles.toggle}
+    >
+      <span
+        className={`${styles.line1} ${styles.lines} ${
+          drawerToggle && styles.close__line1
+        }`}
+      ></span>
+      <span
+        className={`${styles.line2} ${styles.lines} ${
+          drawerToggle && styles.close__line2
+        }`}
+      ></span>
+      <span
+        className={`${styles.line3} ${styles.lines} ${
+          drawerToggle && styles.close__line3
+        }`}
+      ></span>
+    </span>
+  );
+};
+
+Drawer.Toggle = Toggle;
+
+export { Drawer };
