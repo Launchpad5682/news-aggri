b9a686dcabd3d967a21315718f564e93395359be
launchpad5682 <launchpad5682@gmail.com>
2022-08-01 01:21:55 +0530

    feat: App Context aded

--- src/context/app-context.js
+++ src/context/app-context.js
@@ -1,12 +1,43 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { reducer } from "../reducer/reducer";
 
 const AppContext = createContext();
 
 const useAppContext = () => useContext(AppContext);
 
+const initialState = {
+  favorites: [],
+  favoriteToggle: false,
+  source: "all",
+  activeArticle: {},
+};
+
 const AppProvider = ({ children }) => {
+  const [{ favorites, favoriteToggle, source, activeArticle }, dispatch] =
+    useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const cachedFavorites = localStorage.getItem("favorites");
+    if (cachedFavorites !== null) {
+      dispatch({
+        type: "SET_FAVORITES",
+        payload: { favorites: JSON.parse(cachedFavorites).favorites },
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (favorites.length > 0) {
+      localStorage.setItem("favorites", JSON.stringify({ favorites }));
+    }
+  }, [favorites]);
+
   return (
-    <AppContext.Provider value={{ value: 0 }}>{children}</AppContext.Provider>
+    <AppContext.Provider
+      value={{ favorites, favoriteToggle, source, activeArticle, dispatch }}
+    >
+      {children}
+    </AppContext.Provider>
   );
 };
 
